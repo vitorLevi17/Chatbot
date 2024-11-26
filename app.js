c47e965b3978fe54a75451e4c55559908a26fc34
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2024-11-26 17:23:17 -0300

    Refazer iniChat()

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(cors());
 app.use(express.json());
 app.use('/static', express.static(join(__dirname, 'static'), { extensions: ['css', 'svg', 'js'] }));
 
-iniChat()
+
 
 app.get('/', (req, res) => {
-  
+  iniChat()  
   res.sendFile(join(__dirname, 'templates', 'chat.html'));
 });
 
